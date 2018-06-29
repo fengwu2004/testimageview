9143b9f4f1ce2e17ab6297d57322c8624157bd83
晏立 <fengwu2006@gmail.com>
2018-06-29 17:51:28 +0800

    save

---
+++ testimageview/UIImageView+asyncurl.h
@@ -0,0 +1,13 @@
+//
+//  UIImageView+asyncurl.h
+//  testimageview
+//
+//  Created by ky on 2018/6/29.
+//  Copyright © 2018 yellfun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (asyncurl)
+
+@end
---
+++ testimageview/UIImageView+asyncurl.m
@@ -0,0 +1,13 @@
+//
+//  UIImageView+asyncurl.m
+//  testimageview
+//
+//  Created by ky on 2018/6/29.
+//  Copyright © 2018 yellfun. All rights reserved.
+//
+
+#import "UIImageView+asyncurl.h"
+
+@implementation UIImageView (asyncurl)
+
+@end
